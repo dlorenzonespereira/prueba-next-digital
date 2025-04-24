b4bd4db538e8f7f69727d18106ca61d0c3c3c9c2
Diego Lorenzo <dlorenzonespereira@gmail.com>
2025-04-24 10:30:08 +0200

    fix: change getUserById parameter type from string to number

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -2,5 +2,5 @@ import { type User } from '../models/User';
 
 export interface UserRepository {
   getUsers: () => Promise<User[]>;
-  getUserById: (id: string) => Promise<User>;
+  getUserById: (id: number) => Promise<User>;
 }
