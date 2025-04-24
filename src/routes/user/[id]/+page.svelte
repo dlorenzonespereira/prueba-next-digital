781cb62577b9740d148628a77672cfb644be3d2c
Diego Lorenzo <dlorenzonespereira@gmail.com>
2025-04-24 11:54:59 +0200

    feat: create +page.svelte for user details display

---
+++ src/routes/user/[id]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import UserInfo from '$components/UserInfo.svelte';
+  import { type User } from '$domain/models/User';
+
+  export let data: { user: User };
+</script>
+
+<svelte:head>
+  <title>Detalles del usuario {data?.user?.name}</title>
+</svelte:head>
+
+<UserInfo user={data?.user} />
