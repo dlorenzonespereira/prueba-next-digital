25372f9f5f6cb0526b736201626443e7b227fb3d
Diego Lorenzo <dlorenzonespereira@gmail.com>
2025-04-24 11:32:39 +0200

    feat: add +page.svelte to display user list

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import UsersList from '$components/UsersList.svelte';
+  import { type User } from '$domain/models/User';
+
+  export let data: { users: User[] };
+</script>
+
+<svelte:head>
+  <title>Listado de usuarios</title>
+</svelte:head>
+
+<UsersList users={data.users} />
