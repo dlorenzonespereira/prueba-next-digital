e93ee5fca18b01109bb8df2d098c2196e33cccfe
Diego Lorenzo <dlorenzonespereira@gmail.com>
2025-04-24 11:32:19 +0200

    feat: create UsersList component to display a list of users

---
+++ src/infrastructure/ui/UsersList.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { type User } from '$domain/models/User';
+  import UserCard from '$infrastructure/ui/UserCard.svelte';
+
+  export let users: User[];
+</script>
+
+{#if users.length === 0}
+  <li>Sin resultados</li>
+{:else}
+  <ul class="user-list">
+    {#each users as user}
+      <li>
+        <UserCard {user} />
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+<style lang="postcss">
+  .user-list {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    padding: 20px;
+    gap: 20px;
+  }
+
+  @media (max-width: 768px) {
+    .user-list {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media (max-width: 480px) {
+    .user-list {
+      grid-template-columns: 1fr;
+    }
+  }
+</style>
