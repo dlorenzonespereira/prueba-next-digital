3439a44e86e602e3b0ced123b27780337a0586e3
Diego Lorenzo <dlorenzonespereira@gmail.com>
2025-04-24 10:09:25 +0200

    feat: add Svelte configuration file for project setup

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      precompress: true,
+    }),
+    alias: {
+      $components: 'src/infrastructure/ui/*',
+      $domain: 'src/domain/*',
+      $infrastructure: 'src/infrastructure/*',
+    },
+  },
+  compilerOptions: {
+    customElement: true,
+  },
+  preprocess: vitePreprocess({ postcss: true }),
+};
+
+export default config;
